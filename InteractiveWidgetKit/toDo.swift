1b4263b0193f508af760f9c978ca02bf344d0eed
Sabricetin <sabriyazilim@gmail.com>
2024-10-09 17:03:52 +0300

    first commit

---
+++ InteractiveWidgetKit/toDo.swift
@@ -0,0 +1,8 @@
+//
+//  toDo.swift
+//  InteractiveWidgetKit
+//
+//  Created by Sabri Çetin on 9.10.2024.
+//
+
+import Foundation
---
+++ ToDoWidget/ToDoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoWidgetBundle.swift
+//  ToDoWidget
+//
+//  Created by Sabri Çetin on 9.10.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ToDoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ToDoWidget()
+    }
+}
